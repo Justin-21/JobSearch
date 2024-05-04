1716646ce0529eed3f8da4376b282485905d4498
Harshit Tiwari <141118131+HarshitTiwari0710@users.noreply.github.com>
2024-05-04 18:14:11 +0530

    Added a job cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,58 @@
-import './App.css';
-import JobCards from './components/JobCards/index.tsx';
-import Filter from "./components/Filters/index.tsx"
+import "./App.css";
+import JobCards from "./components/JobCards/index.tsx";
+import Filter from "./components/Filters/index.tsx";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [jobData, setjobData] = useState([]);
+
+  const restructureData = (data) => {
+    for (let key in data) {
+      if (data[key] === null) {
+        return false;
+      }
+    }
+    return true;
+  };
+  const fetchData = (data) => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    const body = JSON.stringify({
+      limit: data,
+      offset: 0,
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body,
+    };
+
+    fetch(
+      "https://api.weekday.technology/adhoc/getSampleJdJSON",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        let resultArray = result.jdList.filter((obj) => restructureData(obj));
+        setjobData(resultArray);
+      });
+  };
+  useEffect(() => {
+    // window.addEventListener("scroll", () => {
+    //   if (
+    //     window.innerHeight + document.documentElement.scrollTop ===
+    //     document.documentElement.offsetHeight
+    //   ) {
+    //       fetchData(10);
+    //   }
+    // });
+    fetchData(10);
+  },[]);
   return (
     <div className="App">
-      <JobCards/>
-      <Filter/>
+      <JobCards jobData={jobData} />
+      {/* <Filter /> */}
     </div>
   );
 }
--- src/components/JobCards/index.tsx
+++ src/components/JobCards/index.tsx
@@ -1,7 +1,123 @@
-import React from 'react'
+import { Box, Button, Card, CardContent, Typography } from "@mui/material";
+import React from "react";
+import "./index.css";
+import checkIcon from "../../assets/check.png";
+import highVoltageIcon from "../../assets/highVoltageIcon.png";
 
-export default function JobCards() {
+export default function JobCards(props: any) {
+  const capitalize = (str: any) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  };
   return (
-    <div>Job cards Components</div>
-  )
+    <div className="container">
+      {props.jobData.map((item: any) => {
+        return (
+          <Box key={item.jdUid} sx={{ minWidth: 350, maxWidth: 400 }}>
+            <Card
+              variant="outlined"
+              style={{ borderRadius: "16px", padding: 10 }}
+            >
+              <CardContent className="mainContainer">
+                <div className="leftCard">
+                  <img src={item.logoUrl} alt="" height={38} width={28} />
+                  <div className="innerCard">
+                    <Typography
+                      textAlign={"start"}
+                      color="text.secondary"
+                      fontSize={14}
+                    >
+                      {item.companyName}
+                    </Typography>
+                    <Typography
+                      textAlign={"start"}
+                      color="text.primary"
+                      fontSize={14}
+                    >
+                      {capitalize(item.jobRole)}
+                    </Typography>
+                    <Typography
+                      textAlign={"start"}
+                      color="text.primary"
+                      fontSize={12}
+                      fontWeight={500}
+                    >
+                      {capitalize(item.location)}
+                    </Typography>
+                  </div>
+                </div>
+                <div className="salaryCard">
+                  <Typography fontSize={14} color="text.secondary">
+                    Estimated Salary:
+                  </Typography>
+                  <span style={{ fontSize: "14px" }}>
+                    ₹{item.minJdSalary} - {item.maxJdSalary} LPA
+                  </span>
+                  <img src={checkIcon} height={16} width={16} alt="" />
+                </div>
+                <div className="aboutUs">
+                  <Typography variant="body2" textAlign={"start"}>
+                    About Company:
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    textAlign="start"
+                    fontWeight={600}
+                  >
+                    About us
+                  </Typography>
+                  <Typography variant="body2" gutterBottom textAlign={"start"}>
+                    {item.jobDetailsFromCompany}
+                  </Typography>
+                </div>
+                <div className="experience">
+                  <Typography
+                    variant="body2"
+                    color="text.secondary"
+                    letterSpacing={1.5}
+                    fontSize={12}
+                  >
+                    Minimum experience:
+                  </Typography>
+                  <Typography variant="body2" fontSize={14}>
+                    {item.minExp} {item.minExp > 1 ? "years" : "year"}
+                  </Typography>
+                </div>
+                <Button
+                  style={{
+                    backgroundColor: "#54efc3",
+                    color: "black",
+                    borderRadius: "8px",
+                  }}
+                  fullWidth
+                  href={item.jdLink}
+                  target="_blank"
+                >
+                  <div className="btn">
+                    <img src={highVoltageIcon} alt="" width={20} height={20} />
+                    <span>Easy Apply</span>
+                  </div>
+                </Button>
+                <Button
+                  style={{
+                    backgroundColor: "#4943da",
+                    color: "white",
+                    borderRadius: "8px",
+                    textTransform: "none",
+                  }}
+                  fullWidth
+                  variant="text"
+                  href={item.jdLink}
+                  target="_blank"
+                >
+                  <div className="btn">
+                    <span>Unlock referral asks</span>
+                  </div>
+                </Button>
+              </CardContent>
+            </Card>
+          </Box>
+        );
+      })}
+    </div>
+  );
 }
